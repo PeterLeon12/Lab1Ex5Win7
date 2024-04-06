e6fd7b8c641a1454b4dbeb8243664aecf60f8e33
user <user@DESKTOP-FN026Q4>
2024-04-06 15:34:04 +0300

    Add project files.

---
+++ Lab1Ex5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ex 5
+//Se citesc doua numere de la tastatura, x, y. Scrieti un program care va afisa cele
+//doua valori in ordine crescatoare.
+//• Exemplu: citim 9,0 Afisam : 0 9 
+
+Console.WriteLine("Scrieti primul numar");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Scriteti al doilea numar");
+int n2 = int.Parse(Console.ReadLine());
+
+
+if  (n1 > n2)
+{
+    Console.WriteLine("Ordinea numerelor este");
+    Console.WriteLine(n2 + " " + n1);
+}
+else if (n1 < n2)
+{
+    Console.WriteLine("Ordinea numerelor este");
+    Console.WriteLine(n1 + " " + n2);
+}
+else
+{
+    Console.WriteLine("Numerele sunt egale");
+}
